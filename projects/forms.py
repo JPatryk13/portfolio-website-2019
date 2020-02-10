8aa3e445acaf1b930d401c91083c17be6154d189
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-02-10 03:59:49 +0000

    Contact back-end is now working (locally)

--- projects/forms.py
+++ projects/forms.py
@@ -2,5 +2,27 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea, max_length=500)
+    required_css_class = "form-control"
+
+    name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'John'
+        })
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'john@example.com'
+        })
+    )
+    message = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Hellow World!'
+        }),
+        max_length=500
+    )
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-from django.http import HttpResponseRedirect
-from django.core.mail import send_mail
-from django.urls import reverse
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
 
 from .models import Project
 from .forms import ContactForm
@@ -17,19 +16,57 @@ def about(request):
 
 
 def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(data=request.POST)
+    # Get contact form (name, email, message)
+    form_class = ContactForm
 
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            message = "{0} has sent you a new message:\n\n{1}".format(email[0:email.find('@')], form.cleaned_data['message'])
-            send_mail('New Enquiry', message, email, ['paj24@aber.ac.uk'])
+    if request.method == 'POST':    # If the submit button was pressed...
+        form = form_class(data=request.POST)    # ...extract the data from the request into 'form'
 
-            return render(request, 'sent_success.html')
-    else:
-        form = ContactForm()
+        #
+        #                         HttpRequest.POST
+        # A dictionary-like object containing all given HTTP POST parameters,
+        #           providing that the request contains form data.
+        #
+        #                       dict.get(key, value)
+        # The get() method returns the value for the specified key if key is
+        #                          in dictionary.
+        #
+        # email = EmailMessage(
+        #     subject=      'Hello',
+        #     body=         'Body goes here',
+        #     from_email=   'from@example.com',
+        #     to=           ['to1@example.com', 'to2@example.com'],
+        #     bcc=          ['bcc@example.com'],
+        #     reply_to=     ['another@example.com'],
+        #     headers=      {'Message-ID': 'foo'},
+        # )
+        #
+        if form.is_valid() and valid_email:     # If the content of the form (extracted data) is valid...
+            name = request.POST.get('name', '')     # ...extract the name
+            email = request.POST.get('email', '')    # ...extract the email
+            message = request.POST.get('message', '')   # ...extract the message
 
-    return render(request, 'contact.html', {'form': form})
+            # Email the profile with the contact information
+            template = get_template('contact_template.txt')     # Grab template for message output
+            context = {
+                'name': name,
+                'email': email,
+                'message': message,
+            }   # Organise user input into context dictionary
+            content = template.render(context)  # Feed context into contact_template
+
+            email_message = EmailMessage(
+                subject='New message.',
+                body=content,
+                from_email=email,
+                to=['robermaselko@gmail.com'],
+                headers={'Reply-To': email}
+            )   # Build a message
+            email_message.send()    # And send it
+            return redirect('contact')  # Get the user back to the contact page
+
+    # Return (render) contact page with form_class as form (context)
+    return render(request, 'contact.html', {'form': form_class})
 
 
 class ProjectListView(generic.ListView):
